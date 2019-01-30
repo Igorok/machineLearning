f785049cf1c953d7aa7ea11ccc5fc9e0d53d6eab
Igorok <igordubinin1@yandex.ru>
2019-01-30 23:45:52 +0300

    begining

---
+++ examples/numpy_examples.py
@@ -0,0 +1,7 @@
+import numpy as np
+a = np.array([0,1,2,3,4,5])
+print(
+    'a', a,
+    'a.ndim', a.ndim,
+    'a.shape', a.shape
+)
---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='machineLearning',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        # 'ipython',
+        # 'jupyter',
+        # 'pandas',
+        # 'sympy',
+        # 'nose'
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
