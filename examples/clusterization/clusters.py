7d7416c1494cf59287ccd71953a36cffc562290c
Igor Dubinin <igord@pushok.com>
2019-04-05 06:22:44 -0300

    add comments to function

--- examples/clusterization/clusters.py
+++ examples/clusterization/clusters.py
@@ -1,3 +1,12 @@
+'''
+Кластерный анализ - многомерная статистическая процедура, выполняющая сбор данных, содержащих информацию о выборке объектов, и затем упорядочивающая объекты в сравнительно однородные группы.
+Цели кластеризации:
+    Понимание данных путём выявления кластерной структуры. Разбиение выборки на группы схожих объектов позволяет упростить дальнейшую обработку данных и принятия решений, применяя к каждому кластеру свой метод анализа (стратегия «разделяй и властвуй»).
+    Сжатие данных. Если исходная выборка избыточно большая, то можно сократить её, оставив по одному наиболее типичному представителю от каждого кластера.
+    Обнаружение новизны. Выделяются нетипичные объекты, которые не удаётся присоединить ни к одному из кластеров.
+'''
+
+
 from PIL import Image,ImageDraw
 
 def readfile(filename):
@@ -15,7 +24,6 @@ def readfile(filename):
         data.append([float(x) for x in p[1:]])
     return rownames,colnames,data
 
-
 from math import sqrt
 
 def pearson(v1,v2):
@@ -25,7 +33,7 @@ def pearson(v1,v2):
 
     # Sums of the squares
     sum1Sq=sum([pow(v,2) for v in v1])
-    sum2Sq=sum([pow(v,2) for v in v2])	
+    sum2Sq=sum([pow(v,2) for v in v2])
 
     # Sum of the products
     pSum=sum([v1[i]*v2[i] for i in range(len(v1))])
@@ -37,6 +45,10 @@ def pearson(v1,v2):
 
     return 1.0-num/den
 
+'''
+Алгоритм иерархической кластеризации строит иерархию групп, объединяя на каждом шаге две самые похожие группы. В начале каждая
+группа состоит из одного элемента, в данном случае – одного блога. На каждой итерации вычисляются попарные расстояния между группами, и группы, оказавшиеся самыми близкими, объединяются в новую группу. Так повторяется до тех пор, пока не останется всего одна группа.
+'''
 class bicluster:
     def __init__(self,vec,left=None,right=None,distance=0.0,id=None):
         self.left=left
@@ -60,7 +72,7 @@ def hcluster(rows,distance=pearson):
         for i in range(len(clust)):
             for j in range(i+1,len(clust)):
                 # distances is the cache of distance calculations
-                if (clust[i].id,clust[j].id) not in distances: 
+                if (clust[i].id,clust[j].id) not in distances:
                     distances[(clust[i].id, clust[j].id)] = distance(clust[i].vec, clust[j].vec)
 
                 d = distances[(clust[i].id, clust[j].id)]
@@ -71,7 +83,7 @@ def hcluster(rows,distance=pearson):
 
         # calculate the average of the two clusters
         mergevec=[
-        (clust[lowestpair[0]].vec[i]+clust[lowestpair[1]].vec[i])/2.0 
+        (clust[lowestpair[0]].vec[i]+clust[lowestpair[1]].vec[i])/2.0
         for i in range(len(clust[0].vec))]
 
         # create the new cluster
@@ -108,6 +120,10 @@ def printclust(clust,labels=None,n=0):
 # printclust(clust, labels = blognames)
 
 
+'''
+Интерпретацию кластеров можно облегчить, если изобразить их в виде дендрограммы. Результаты иерархической кластеризации обычно так и представляются.
+'''
+
 def getheight(clust):
     # Is this an endpoint? Then the height is just 1
     if clust.left==None and clust.right==None: return 1
@@ -138,7 +154,7 @@ def drawdendrogram(clust,labels,jpeg='clusters.jpg'):
     img=Image.new('RGB',(w,h),(255,255,255))
     draw=ImageDraw.Draw(img)
 
-    draw.line((0,h/2,10,h/2),fill=(255,0,0))    
+    draw.line((0,h/2,10,h/2),fill=(255,0,0))
 
     # Draw the first node
     drawnode(draw,clust,10,(h/2),scaling,labels)
@@ -152,22 +168,29 @@ def drawnode(draw,clust,x,y,scaling,labels):
         bottom=y+(h1+h2)/2
         # Line length
         ll=clust.distance*scaling
-        # Vertical line from this cluster to children    
-        draw.line((x,top+h1/2,x,bottom-h2/2),fill=(255,0,0))    
+        # Vertical line from this cluster to children
+        draw.line((x,top+h1/2,x,bottom-h2/2),fill=(255,0,0))
 
         # Horizontal line to left item
-        draw.line((x,top+h1/2,x+ll,top+h1/2),fill=(255,0,0))    
+        draw.line((x,top+h1/2,x+ll,top+h1/2),fill=(255,0,0))
 
         # Horizontal line to right item
-        draw.line((x,bottom-h2/2,x+ll,bottom-h2/2),fill=(255,0,0))        
+        draw.line((x,bottom-h2/2,x+ll,bottom-h2/2),fill=(255,0,0))
 
-        # Call the function to draw the left and right nodes    
+        # Call the function to draw the left and right nodes
         drawnode(draw,clust.left,x+ll,top+h1/2,scaling,labels)
         drawnode(draw,clust.right,x+ll,bottom-h2/2,scaling,labels)
-    else:   
+    else:
         # If this is an endpoint, draw the item label
         draw.text((x+5,y-7),labels[clust.id],(0,0,0))
 
+
+
+'''
+Часто бывает необходимо выполнить кластеризацию одновременно по строкам и столбцам. В маркетинговых исследованиях интересно сгруппировать людей с целью выявления общих демографических признаков или предпочитаемых товаров, а быть может, для того чтобы выяснить, на каких полках размещены товары, которые обычно покупают вместе. В наборе данных о блогах столбцы представляют слова, и можно поинтересоваться, какие слова часто употребляют вместе.
+Простейший способ решить эту задачу с помощью уже написанных функций – повернуть весь набор данных, так чтобы столбцы (слова)
+стали строками. Тогда списки чисел в каждой строке покажут, сколько раз данное слово встречалось в каждом блоге.
+'''
 def rotatematrix(data):
     newdata=[]
     for i in range(len(data[0])):
@@ -175,22 +198,28 @@ def rotatematrix(data):
         newdata.append(newrow)
     return newdata
 
-import random
+blognames, words, data = readfile('blogdata.txt')
+data = rotatematrix(data)
+wordclust = hcluster(data)
+drawdendrogram(wordclust, labels=words, jpeg='wordclust.jpg')
 
+
+
+import random
 def kcluster(rows,distance=pearson,k=4):
     # Determine the minimum and maximum values for each point
-    ranges=[(min([row[i] for row in rows]),max([row[i] for row in rows])) 
+    ranges=[(min([row[i] for row in rows]),max([row[i] for row in rows]))
     for i in range(len(rows[0]))]
 
     # Create k randomly placed centroids
-    clusters=[[random.random()*(ranges[i][1]-ranges[i][0])+ranges[i][0] 
+    clusters=[[random.random()*(ranges[i][1]-ranges[i][0])+ranges[i][0]
     for i in range(len(rows[0]))] for j in range(k)]
-    
+
     lastmatches=None
     for t in range(100):
         print('Iteration %d' % t)
         bestmatches=[[] for i in range(k)]
-        
+
         # Find which centroid is the closest for each row
         for j in range(len(rows)):
             row = rows[j]
@@ -203,7 +232,7 @@ def kcluster(rows,distance=pearson,k=4):
         # If the results are the same as last time, this is complete
         if bestmatches==lastmatches: break
         lastmatches=bestmatches
-        
+
         # Move the centroids to the average of their members
         for i in range(k):
             avgs=[0.0]*len(rows[0])
@@ -214,48 +243,48 @@ def kcluster(rows,distance=pearson,k=4):
                 for j in range(len(avgs)):
                     avgs[j]/=len(bestmatches[i])
                 clusters[i]=avgs
-        
+
     return bestmatches
 
 def tanamoto(v1,v2):
     c1,c2,shr=0,0,0
-    
+
     for i in range(len(v1)):
         if v1[i]!=0: c1+=1 # in v1
         if v2[i]!=0: c2+=1 # in v2
         if v1[i]!=0 and v2[i]!=0: shr+=1 # in both
-    
+
     return 1.0-(float(shr)/(c1+c2-shr))
 
 def scaledown(data,distance=pearson,rate=0.01):
     n=len(data)
 
     # The real distances between every pair of items
-    realdist=[[distance(data[i],data[j]) for j in range(n)] 
+    realdist=[[distance(data[i],data[j]) for j in range(n)]
                 for i in range(0,n)]
 
     # Randomly initialize the starting points of the locations in 2D
     loc=[[random.random(),random.random()] for i in range(n)]
     fakedist=[[0.0 for j in range(n)] for i in range(n)]
-    
+
     lasterror=None
     for m in range(0,1000):
         # Find projected distances
         for i in range(n):
             for j in range(n):
-                fakedist[i][j]=sqrt(sum([pow(loc[i][x]-loc[j][x],2) 
+                fakedist[i][j]=sqrt(sum([pow(loc[i][x]-loc[j][x],2)
                                         for x in range(len(loc[i]))]))
-    
+
         # Move points
         grad=[[0.0,0.0] for i in range(n)]
-        
+
         totalerror=0
         for k in range(n):
             for j in range(n):
                 if j==k: continue
                 # The error is percent difference between the distances
                 errorterm=(fakedist[j][k]-realdist[j][k])/realdist[j][k]
-                
+
                 # Each point needs to be moved away from or towards the other
                 # point in proportion to how much error it has
                 grad[k][0]+=((loc[k][0]-loc[j][0])/fakedist[j][k])*errorterm
@@ -268,7 +297,7 @@ def scaledown(data,distance=pearson,rate=0.01):
         # If the answer got worse by moving the points, we are done
         if lasterror and lasterror<totalerror: break
         lasterror=totalerror
-        
+
         # Move each of the points by the learning rate times the gradient
         for k in range(n):
             loc[k][0]-=rate*grad[k][0]
@@ -289,6 +318,6 @@ def draw2d(data,labels,jpeg='mds2d.jpg'):
 
 
 
-blognames, words, data = readfile('blogdata.txt')
-clust = hcluster(data)
-drawdendrogram(clust, blognames, jpeg='blogclust.jpg')
+# blognames, words, data = readfile('blogdata.txt')
+# clust = hcluster(data)
+# drawdendrogram(clust, blognames, jpeg='blogclust.jpg')
