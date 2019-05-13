3422ecf376d9b0dc5c85478fc90edc41cfef06e8
Igorok <igordubinin1@yandex.ru>
2019-05-13 23:46:33 +0300

    begin genetic

--- examples/optimization/optimization.py
+++ examples/optimization/optimization.py
@@ -291,4 +291,68 @@ Seymour       BOS 17:11-18:30 $108 13:39-15:30 $ 74
 '''
 Генетические алгоритмы
 Еще один класс методов оптимизации, также навеянный природой, называется генетическими алгоритмами. Принцип их работы состоит в том, чтобы создать набор случайных решений, который называется популяцией. На каждом шаге оптимизации целевая функция вычисляется для всей популяции, в результате чего получается ранжированный список решений.
+Проранжировав решения, мы создаем новую популяцию, которая называется следующим поколением. Сначала в новую популяцию включаются самые лучшие решения из текущей. Этот процесс называется элитизмом. Кроме них, в следующую популяцию входят совершенно новые решения, получающиеся путем модификации наилучших. Модифицировать решения можно двумя способами. Более простой называется мутацией; обычно это небольшое, простое, случайное изменение существующего решения.
+Модифицировать решения можно двумя способами. Более простой называется мутацией; обычно это небольшое, простое, случайное изменение существующего решения.
+Другой способ называется скрещиванием (или кроссовером). Состоит он в том, что мы берем какие-нибудь два из лучших решений и как-то комбинируем их.
+Размер новой популяции обычно совпадает с размером предыдущей, а создается она путем случайных мутаций и скрещиваний лучших решений.
 '''
+
+def geneticoptimize(domain, costf, popsize = 50, step = 1, mutprob = 0.2, elite = 0.2, maxiter = 100):
+    # Mutation Operation
+    def mutate(vec):
+        i = random.randint(0, len(domain) - 1)
+        if random.random() < 0.5 and vec[i] > domain[i][0]:
+            return vec[0 : i] + [vec[i] - step] + vec[i + 1:] 
+        elif vec[i] < domain[i][1]:
+            return vec[0 : i] + [vec[i] + step] + vec[i + 1:]
+
+    # Crossover Operation
+    def crossover(r1, r2):
+        i = random.randint(1, len(domain) - 2)
+        return r1[0:i] + r2[i:]
+
+    # Build the initial population
+    pop = []
+    for i in range(popsize):
+        vec = [
+            random.randint(domain[i][0], domain[i][1]) 
+            for i in range(len(domain))
+        ]
+        pop.append(vec)
+
+    # How many winners from each generation?
+    topelite = int(elite * popsize)
+
+    # Main loop 
+    for i in range(maxiter):
+        scores = [(costf(v), v) for v in pop]
+        scores.sort()
+        ranked = [v for (s, v) in scores]
+
+        # Start with the pure winners
+        pop = ranked[0 : topelite]
+
+        # Add mutated and bred forms of the winners
+        while len(pop) < popsize:
+            if random.random() < mutprob:
+                # Mutation
+                c = random.randint(0, topelite)
+                pop.append(mutate(ranked[c]))
+            else:
+                # Crossover
+                c1 = random.randint(0, topelite)
+                c2 = random.randint(0, topelite)
+                pop.append(crossover(ranked[c1], ranked[c2]))
+
+        # Print current best score
+        print(scores[0][0])
+
+    print ('scores[0][1]', scores[0][1])
+
+    return scores[0][1]
+
+
+
+
+geneticoptimize(domain, schedulecost)
+printschedule(s)
